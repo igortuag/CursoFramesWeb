2004166f884429ffabc513d095955b905341d9aa
igortuag <igortuag@gmail.com>
2019-08-25 15:59:38 -0300

    aula89

--- angular1/app/common/factories/tabsFactory.js
+++ angular1/app/common/factories/tabsFactory.js
@@ -8,6 +8,13 @@
             tabCreate = false,
             tabUpdate = false,
             tabDelete = false
-        })
+        }) {
+            owner.tabList = tabList
+            owner.tabCreate = tabCreate
+            owner.tabUpdate = tabUpdate
+            owner.tabDelete = tabDelete
+        }
+
+        return { show }
     }
 })()
