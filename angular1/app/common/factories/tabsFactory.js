099048d4310b9b9e35edfcacf97fb66e0086bc96
igortuag <igortuag@gmail.com>
2019-08-25 15:45:30 -0300

    aula 88

---
+++ angular1/app/common/factories/tabsFactory.js
@@ -0,0 +1,13 @@
+(function(){
+    angular.module('primeiraApp').factory('tabs', [ TabsFactory ])
+
+    function TabsFactory() {
+        
+        function show(owner, {
+            tabList = false,
+            tabCreate = false,
+            tabUpdate = false,
+            tabDelete = false
+        })
+    }
+})()
