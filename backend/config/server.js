446cdf4fb9d4e46ed315ecfbc6d6eb271aa2dabc
igortuag <igortuag@gmail.com>
2019-08-24 23:02:55 -0300

    aula79

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,9 +3,11 @@ const port = 3003
 const boodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 server.use(boodyParser.urlencoded({ extended: true }))
 server.use(boodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
