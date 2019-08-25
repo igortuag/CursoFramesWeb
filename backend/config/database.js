76c213c597242ce1ecd8a0b12261a6caddd79b83
igortuag <igortuag@gmail.com>
2019-08-25 14:44:44 -0300

    aula86

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance', { useNewUrlParser: true })
 
 mongoose.Error.messages.general.require = "O atributo '{PATH}' é obritório."
-mongoose.Error.messages.Number.min = "O valor: '{VALUE}' é menor que o liminito minimo de '{MIN}."
-mongoose.Error.messages.Number.max = "O valor: '{VALUE}' é menor que o liminito minimo de '{MAX}."
+mongoose.Error.messages.Number.min = "O valor: '{VALUE}' é menor que o liminito mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O valor: '{VALUE}' é maior que o liminito máximo de '{MAX}'."
